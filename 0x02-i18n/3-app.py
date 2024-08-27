152e990596aa8eed9a4da51cdb3ab453152ec907
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-08-27 14:22:54 -0700

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """Parametrize templates"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
@@ -17,14 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """Determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/', strict_slashes=False)
-def index() -> str:
+def index():
     """Render the index page"""
     return render_template('3-index.html')
 
